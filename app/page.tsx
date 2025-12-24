5173dd3fe2617e243c8f506694213c16e63838b3
charu2123 <charuparnwal@gmail.com>
2025-12-24 18:45:37 +0530

    Restore app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
 
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Next.js User CRUD</h1>
+      <p>Welcome! Go to /users to see users list.</p>
+    </main>
+  );
+}
